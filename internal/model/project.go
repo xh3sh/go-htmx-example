3ad877876550225fb19c4a8a3d51d8222d1a83ce
zetsqio <stea.xd@yandex.ru>
2025-07-15 22:09:56 +0300

    style: update cards layout

--- internal/model/project.go
+++ internal/model/project.go
@@ -3,6 +3,7 @@ package model
 type Project struct {
 	ImageURL    string   `yaml:"image_url"`
 	ProjectURL  string   `yaml:"project_url"` // Add ProjectURL field
+	SwaggerURL  string   `yaml:"swagger_url"`
 	Title       string   `yaml:"title"`
 	Description string   `yaml:"description"`
 	Link        string   `yaml:"link"`
